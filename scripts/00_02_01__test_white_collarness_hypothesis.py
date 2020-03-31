75c4ff4a7263b24fe8e77e45a9bd2850318974ff
Syed Arefinul Haque <syedarehaq@gmail.com>
2020-03-30 20:39:29 -0400

    change the line endings to unix

--- scripts/00_02_01__test_white_collarness_hypothesis.py
+++ scripts/00_02_01__test_white_collarness_hypothesis.py
@@ -1,1 +1,77 @@
-import pandas as pdimport datetimeimport matplotlib.pyplot as pltimport scipy.stats as stats# %%output_code = "00_01_01"now = datetime.datetime.now().strftime("%Y%m%d")# %%df_white_collarness_ranking = pd.read_csv("../inputs/internal/00_01_01_white_collarness_ranking.csv")# %%## Now reading the evidation dataset compiled by Tareqdf_evidation = pd.read_csv("../inputs/external/Evidation Data - Sheet1.csv")# %%evidation_work_from_home_dict = df_evidation.set_index("State").to_dict()["Rank - Started working from home"]evidation_avoided_large_gathering_dict = df_evidation.set_index("State").to_dict()["Rank - Avoided large gatherings"]state_name_to_state_code_dict = df_evidation.set_index("State").to_dict()["State Code"]# %%white_collarness_dict = df_white_collarness_ranking.set_index("state_name").to_dict()["ranking_based_on_white_colarness"]# %%common_states = set(white_collarness_dict) & set(evidation_work_from_home_dict)# %%state_names = []state_codes = []evidation_work_from_home_ranks = []white_collarness_ranks = []for state in common_states:    evidation_work_from_home_ranks.append(evidation_work_from_home_dict[state])    white_collarness_ranks.append(white_collarness_dict[state])    state_names.append(state)    state_codes.append(state_name_to_state_code_dict[state])# %%tau, p_value_tau = stats.kendalltau(evidation_work_from_home_ranks, white_collarness_ranks)# %%rho, p_value_spearman = stats.spearmanr(evidation_work_from_home_ranks,white_collarness_ranks)# %%fig,ax = plt.subplots(figsize = (10,8))ax.plot(evidation_work_from_home_ranks,white_collarness_ranks, "o", color = "green")ax.text(0,44, 'spearman rank corr, r = %.2f\np value %.3f\nkendal tau rank corr, r = %.2f\np value %.3f\n' %(rho, p_value_spearman, tau, p_value_tau), fontsize=13)for i in range(len(state_codes)):    ax.annotate(state_names[i],(evidation_work_from_home_ranks[i]+0.1,white_collarness_ranks[i]+0.1))ax.set_xlabel("Evidation work form home ranking for state \n(lower rank means more percentage of answered that they stayed at home)", fontsize=13)ax.set_ylabel("Ranking of states based on White collar job \n(lower rank means there were more white collar job in that state)", fontsize=13)fig.tight_layout()fig.savefig("../outputs/figures/%s_rank_correlation_between_white_collarness_and_work_from_home_%s.png" %(output_code,now), dpi = 150)# %%###################################### Avoided large gathering with white collarness# %%state_names = []state_codes = []evidation_ranks = []white_collarness_ranks = []for state in common_states:    evidation_ranks.append(evidation_avoided_large_gathering_dict[state])    white_collarness_ranks.append(white_collarness_dict[state])    state_names.append(state)    state_codes.append(state_name_to_state_code_dict[state])# %%tau, p_value_tau = stats.kendalltau(evidation_ranks, white_collarness_ranks)# %%rho, p_value_spearman = stats.spearmanr(evidation_ranks,white_collarness_ranks)# %%fig,ax = plt.subplots(figsize = (10,8))ax.plot(evidation_ranks,white_collarness_ranks, "o", color = "green")ax.text(0,44, 'spearman rank corr, r = %.2f\np value %.3f\nkendal tau rank corr, r = %.2f\np value %.3f\n' %(rho, p_value_spearman, tau, p_value_tau), fontsize=13)for i in range(len(state_codes)):    ax.annotate(state_names[i],(evidation_ranks[i]+0.1,white_collarness_ranks[i]+0.1))ax.set_xlabel("Evidation avoided large gathering ranking for state \n(lower rank means more percentage of people answered they avoided large gathering)", fontsize=13)ax.set_ylabel("Ranking of states based on White collar job \n(lower rank means there were more white collar job in that state)", fontsize=13)fig.tight_layout()fig.savefig("../outputs/figures/%s_rank_correlation_between_white_collarness_and_avoided_large_gathering_%s.png" %(output_code,now), dpi = 150)
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+# %%
+output_code = "00_01_01"
+now = datetime.datetime.now().strftime("%Y%m%d")
+# %%
+df_white_collarness_ranking = pd.read_csv("../inputs/internal/00_01_01_white_collarness_ranking.csv")
+# %%
+## Now reading the evidation dataset compiled by Tareq
+df_evidation = pd.read_csv("../inputs/external/Evidation Data - Sheet1.csv")
+# %%
+evidation_work_from_home_dict = df_evidation.set_index("State").to_dict()["Rank - Started working from home"]
+evidation_avoided_large_gathering_dict = df_evidation.set_index("State").to_dict()["Rank - Avoided large gatherings"]
+state_name_to_state_code_dict = df_evidation.set_index("State").to_dict()["State Code"]
+# %%
+white_collarness_dict = df_white_collarness_ranking.set_index("state_name").to_dict()["ranking_based_on_white_colarness"]
+
+# %%
+common_states = set(white_collarness_dict) & set(evidation_work_from_home_dict)
+
+# %%
+state_names = []
+state_codes = []
+evidation_work_from_home_ranks = []
+white_collarness_ranks = []
+for state in common_states:
+    evidation_work_from_home_ranks.append(evidation_work_from_home_dict[state])
+    white_collarness_ranks.append(white_collarness_dict[state])
+    state_names.append(state)
+    state_codes.append(state_name_to_state_code_dict[state])
+# %%
+tau, p_value_tau = stats.kendalltau(evidation_work_from_home_ranks, white_collarness_ranks)
+# %%
+rho, p_value_spearman = stats.spearmanr(evidation_work_from_home_ranks,white_collarness_ranks)
+# %%
+fig,ax = plt.subplots(figsize = (10,8))
+ax.plot(evidation_work_from_home_ranks,white_collarness_ranks, "o", color = "green")
+ax.text(0,44, 'spearman rank corr, r = %.2f\np value %.3f\nkendal tau rank corr, r = %.2f\np value %.3f\n' %(rho, p_value_spearman, tau, p_value_tau), fontsize=13)
+for i in range(len(state_codes)):
+    ax.annotate(state_names[i],(evidation_work_from_home_ranks[i]+0.1,white_collarness_ranks[i]+0.1))
+ax.set_xlabel("Evidation work form home ranking for state \n(lower rank means more percentage of answered that they stayed at home)", fontsize=13)
+ax.set_ylabel("Ranking of states based on White collar job \n(lower rank means there were more white collar job in that state)", fontsize=13)
+fig.tight_layout()
+fig.savefig("../outputs/figures/%s_rank_correlation_between_white_collarness_and_work_from_home_%s.png" %(output_code,now), dpi = 150)
+
+# %%
+##################
+##################
+## Avoided large gathering with white collarness
+
+# %%
+state_names = []
+state_codes = []
+evidation_ranks = []
+white_collarness_ranks = []
+for state in common_states:
+    evidation_ranks.append(evidation_avoided_large_gathering_dict[state])
+    white_collarness_ranks.append(white_collarness_dict[state])
+    state_names.append(state)
+    state_codes.append(state_name_to_state_code_dict[state])
+# %%
+tau, p_value_tau = stats.kendalltau(evidation_ranks, white_collarness_ranks)
+# %%
+rho, p_value_spearman = stats.spearmanr(evidation_ranks,white_collarness_ranks)
+# %%
+fig,ax = plt.subplots(figsize = (10,8))
+ax.plot(evidation_ranks,white_collarness_ranks, "o", color = "green")
+ax.text(0,44, 'spearman rank corr, r = %.2f\np value %.3f\nkendal tau rank corr, r = %.2f\np value %.3f\n' %(rho, p_value_spearman, tau, p_value_tau), fontsize=13)
+for i in range(len(state_codes)):
+    ax.annotate(state_names[i],(evidation_ranks[i]+0.1,white_collarness_ranks[i]+0.1))
+ax.set_xlabel("Evidation avoided large gathering ranking for state \n(lower rank means more percentage of people answered they avoided large gathering)", fontsize=13)
+ax.set_ylabel("Ranking of states based on White collar job \n(lower rank means there were more white collar job in that state)", fontsize=13)
+fig.tight_layout()
+fig.savefig("../outputs/figures/%s_rank_correlation_between_white_collarness_and_avoided_large_gathering_%s.png" %(output_code,now), dpi = 150)
+
--- scripts/00_03_01_process_unacast_data.py
+++ scripts/00_03_01_process_unacast_data.py
@@ -1,1 +1,35 @@
-import pandas as pdimport datetimeimport matplotlib.pyplot as pltimport scipy.stats as stats# %%output_code = "00_03_01"now = datetime.datetime.now().strftime("%Y%m%d")# %%df_unacast = pd.read_csv("../inputs/external/untracked/county_level_aggregates_26032020.csv")# %%df_single_date = df_unacast[df_unacast["localeventDate"]=="3/23/20"]# %%df_state_total_population = df_single_date[["state_code","county_population"]].groupby(["state_code"]).sum().rename(columns={'state_code':'state_code','county_population':'state_population'}).reset_index()# %%df_unacast["state_population"] = df_unacast[["localeventDate","state_code","county_population"]].groupby(["state_code","localeventDate"]).transform("sum")# %%df_unacast["diff_weighted_by_county_population"] = df_unacast["county_population"] * df_unacast["daily_distance_diff"] / df_unacast["state_population"]# %%df_weighted_average = df_unacast[["localeventDate","state_code","diff_weighted_by_county_population"]].groupby(["state_code","localeventDate"]).sum().reset_index()df_weighted_average.columns = ["state_code","local_event_date","weighted_avg_distance_diff_by_county_population"]df_weighted_average.set_index("state_code")# %%df_median = df_unacast[["localeventDate","state_code","daily_distance_diff"]].groupby(["state_code","localeventDate"]).median().reset_index()df_median.columns = ["state_code","local_event_date","median_distance_diff_from_counties"]#df_median.set_index("state_code")# %%df_mean = df_unacast[["localeventDate","state_code","daily_distance_diff"]].groupby(["state_code","localeventDate"]).mean().reset_index()df_mean.columns = ["state_code","local_event_date","mean_distance_diff_from_counties"]# %%df_new = df_weighted_average.merge(df_median,  how = "inner")df_new.set_index("state_code")df_new = df_new.merge(df_mean, how = "inner")df_new.set_index("state_code").to_csv("../inputs/internal/untracked/%s_state_level_aggregated_movement_diff_unacast.csv" %output_code)
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+# %%
+output_code = "00_03_01"
+now = datetime.datetime.now().strftime("%Y%m%d")
+# %%
+df_unacast = pd.read_csv("../inputs/external/untracked/county_level_aggregates_26032020.csv")
+# %%
+df_single_date = df_unacast[df_unacast["localeventDate"]=="3/23/20"]
+# %%
+df_state_total_population = df_single_date[["state_code","county_population"]].groupby(["state_code"]).sum().rename(columns={'state_code':'state_code','county_population':'state_population'}).reset_index()
+# %%
+df_unacast["state_population"] = df_unacast[["localeventDate","state_code","county_population"]].groupby(["state_code","localeventDate"]).transform("sum")
+# %%
+df_unacast["diff_weighted_by_county_population"] = df_unacast["county_population"] * df_unacast["daily_distance_diff"] / df_unacast["state_population"]
+# %%
+df_weighted_average = df_unacast[["localeventDate","state_code","diff_weighted_by_county_population"]].groupby(["state_code","localeventDate"]).sum().reset_index()
+df_weighted_average.columns = ["state_code","local_event_date","weighted_avg_distance_diff_by_county_population"]
+df_weighted_average.set_index("state_code")
+# %%
+df_median = df_unacast[["localeventDate","state_code","daily_distance_diff"]].groupby(["state_code","localeventDate"]).median().reset_index()
+df_median.columns = ["state_code","local_event_date","median_distance_diff_from_counties"]
+#df_median.set_index("state_code")
+
+# %%
+df_mean = df_unacast[["localeventDate","state_code","daily_distance_diff"]].groupby(["state_code","localeventDate"]).mean().reset_index()
+df_mean.columns = ["state_code","local_event_date","mean_distance_diff_from_counties"]
+
+# %%
+df_new = df_weighted_average.merge(df_median,  how = "inner")
+df_new.set_index("state_code")
+df_new = df_new.merge(df_mean, how = "inner")
+df_new.set_index("state_code").to_csv("../inputs/internal/untracked/%s_state_level_aggregated_movement_diff_unacast.csv" %output_code)
